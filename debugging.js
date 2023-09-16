b155e2523770df81fee2bc87dfee4a80b4aa033e
wjdavie <wesley.davie@gmail.com>
2023-09-16 19:18:10 -0400

    Fixed the Part 1, 2, and 3 errors.

---
+++ debugging.js
@@ -0,0 +1,15 @@
+const pi = 3.14;
+let radius = 3;
+let area = 0;
+
+function circleArea(radius) {
+const area = radius * radius * pi;
+return area;
+}
+
+//radius = 4;
+area = circleArea(3);
+console.log("Area1:", area)
+
+area = circleArea(4);
+console.log("Area2:", area);
